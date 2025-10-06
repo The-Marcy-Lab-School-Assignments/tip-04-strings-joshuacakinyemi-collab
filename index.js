7686ba3a872b6261c4146de7d55894bd0427a71c
Joshua Akinyemi <joshuacakinyemi@gmail.com>
2025-10-06 10:44:11 -0400

    done work

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ You should not use the reverse method and you should not use the reverse method 
 
 The function should return a NEW string as the output. 
 */
-function reverseString(str) {}
+function reverseString(str) {
+
+    for (let i = str.length - 1; i > 0; i--) {
+        console.log(str[i])
+    }
+}
 
 // Test the function
 console.log(reverseString("Hello")); // Output: "olleH"
